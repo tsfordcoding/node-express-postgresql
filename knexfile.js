2495795d30933a34f614d7b79f4c51959072962d
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-07-18 18:10:59 -0700

    Fix production

--- knexfile.js
+++ knexfile.js
@@ -10,13 +10,23 @@ const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
-    }
+    },
+  },
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 };
